60527ee9cf6c5cdd0c25fab0124b6f0529f143a2
Kenan Casey <kenancasey@gmail.com>
2019-09-25 10:36:38 -0500

    initial commit

---
+++ Queue/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Queue<string> queue = new Queue<string>();
+
+            queue.Add("A");
+            queue.Add("B");
+            queue.Add("C");
+            queue.Add("D");
+
+            Console.WriteLine(queue.Front);
+            Console.WriteLine(queue.Back);
+            Console.WriteLine(queue.Remove());
+            Console.WriteLine(queue.Remove());
+            Console.WriteLine(queue.Remove());
+            Console.WriteLine(queue.Remove());
+            Console.WriteLine(queue.Remove());
+            Console.WriteLine(queue.Remove());
+            Console.WriteLine(queue.Remove());
+            Console.WriteLine(queue.Remove());
+            Console.WriteLine(queue.Remove());
+            Console.WriteLine(queue.Remove());
+            Console.WriteLine(queue.Remove());
+            Console.WriteLine(queue.Remove());
+            Console.WriteLine(queue.Remove());
+        }
+    }
+}
---
+++ Queue/Queue.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queue
+{
+    public class Queue<T>
+    {
+        LinkedList<T> linkedList;
+
+        public Queue()
+        {
+            linkedList = new LinkedList<T>();
+        }
+
+        public int Length => linkedList.Count;
+
+        public void Add(T item)
+        {
+            linkedList.AddLast(new LinkedListNode<T>(item));
+        }
+
+        public T Remove()
+        {
+            if( IsEmpty )
+            {
+                // throw and exception
+                return default(T);
+            }
+
+            T item = linkedList.First.Value;
+            linkedList.RemoveFirst();
+
+            return item;
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return (linkedList.Count == 0);
+            }
+        }
+
+        public T Front
+        {
+            get
+            {
+                if (IsEmpty)
+                {
+                    // throw and exception
+                    return default(T);
+                }
+
+                return linkedList.First.Value;
+            }
+        }
+
+        public T Back
+        {
+            get
+            {
+                if (IsEmpty)
+                {
+                    // throw and exception
+                    return default(T);
+                }
+
+                return linkedList.Last.Value;
+            }
+        }
+    }
+}
